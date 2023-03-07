41dfd3b27acc9677e4d47214eed1f8c1ada8be2a
Yuri <loccckkky@yandex.ru>
2023-03-07 12:56:12 +0400

    Первое задание

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,9 +1,13 @@
 ﻿// Показать натуральные числа от M до N, N и M заданы
 
-void PrintNumber(int m, int n)
-{
-    if (n < m) return;
-    PrintNumber(m, n - 1);
-    Console.Write(n + " ");
-}
-PrintNumber(1, 10);
+Console.Write("Введи N: ");
+int N = int.Parse(Console.ReadLine());
+Console.Write("Введи M: ");
+int M = int.Parse(Console.ReadLine());
+Console.WriteLine($"N={N}, M={M}");
+if (M>N)
+for (int i = N; i <= M; i++)
+    Console.Write($" {i}");
+else
+    for (int i = M; i <= N; i++)
+        Console.Write($" {i}");
