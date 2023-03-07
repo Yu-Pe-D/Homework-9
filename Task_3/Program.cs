4b63c223b360c209a315f1b6f91241d3968cbf90
Yuri <loccckkky@yandex.ru>
2023-03-07 13:18:14 +0400

    Третье задание

---
+++ Task_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найти сумму цифр числа
+Console.Clear();
+Console.Write("Введите целое число: ");
+
+ int a = int.Parse(Console.ReadLine());
+            int s = 0;
+            while (a > 0)
+            {
+ 
+                s = s + a % 10;
+                a = a /10 ;
+ 
+            }
+ Console.WriteLine();
+ Console.WriteLine(s);
+ Console.WriteLine();
