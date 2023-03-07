b6285e08fd1e5de969674717edea0d899705a810
Yuri <loccckkky@yandex.ru>
2023-03-07 13:52:03 +0400

    Пятое задание

---
+++ Task_5/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу возведения числа А в целую стень B
+
+Console.Clear();
+Console.Write("Введите число: ");
+double A = double.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.Write("Введите степень в виде целого числа: ");
+
+double B = double.Parse(Console.ReadLine());
+A = Math.Pow(A, B);
+Console.WriteLine();
+Console.WriteLine("равно "+A+"");
+Console.WriteLine();
