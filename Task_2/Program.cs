e7180ceca58d20deacb88c09d439d0d2c0932a2c
Yuri <loccckkky@yandex.ru>
2023-03-07 13:07:03 +0400

    Второе задание

---
+++ Task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+Console.WriteLine("Введите начальное число M:");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите начальное число N:");
+int numberN = int.Parse(Console.ReadLine());
+
+void GapNumberSum (int numberM, int numberN, int sum)
+{
+    if (numberM > numberN) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    GapNumberSum(numberM, numberN, sum);
+}
+
+GapNumberSum(numberM, numberN, 0);
