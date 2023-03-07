9b1728fa033d0b56c50c18fb4dc399139cd4dd5f
Yuri <loccckkky@yandex.ru>
2023-03-07 15:06:23 +0400

    Шестое задание

---
+++ Task_6/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. Первые два элемента последовательности задаются пользователем
+Console.Clear();
+
+string res = string.Empty;
+int tmp2 = 0;
+
+string FirstNumbers(int a, int b, int n)
+{
+    tmp2 = a + b;
+    a = b;
+    b = tmp2;
+    res += tmp2 + " ";
+    if (n > 1) FirstNumbers(a, b, n - 1);
+
+    return res;
+}
+
+Console.WriteLine(FirstNumbers(1,2,7));
